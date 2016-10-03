6072eca6e126833eebe2d0cf13487868c5bbc3fb
Jennifer Toner <wilhelmena3@gmail.com>
2016-10-03 16:20:02 -0500

    added stats from Products table: amount of unique products, average price and total stock quantity.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,12 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+
+    @unique_products = Product.select(:title).uniq.count
+
+    @average_price = Product.average(:price)
+
+    @total_stock = Product.sum(:stock_quantity)
   end
 
   # GET /products/1
